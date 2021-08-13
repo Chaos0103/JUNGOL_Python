b4fe8d0f9a5b50a133c0ae1190115ee84e34b71f
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-08-13 22:46:13 +0900

    Create 자가진단_04.py

---
+++ LCoder_Python/연산자/자가진단_04.py
@@ -0,0 +1,11 @@
+num1 = int(input())
+num2 = int(input())
+
+result = num1 > num2
+print(num1, '>', num2, '---', result)
+result = num1 < num2
+print(num1, '<', num2, '---', result)
+result = num1 >= num2
+print(num1, '>=', num2, '---', result)
+result = num1 <= num2
+print(num1, '<=', num2, '---', result)
