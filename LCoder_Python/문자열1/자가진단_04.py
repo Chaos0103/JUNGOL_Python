9276886a293d8959b2523f4ab8a215a8b9e2422f
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-08-13 23:28:56 +0900

    Create 자가진단_04.py

---
+++ LCoder_Python/문자열1/자가진단_04.py
@@ -0,0 +1,7 @@
+animal1, sound1 = map(str, input().split())
+animal2, sound2 = map(str, input().split())
+animal3, sound3 = map(str, input().split())
+
+print(animal1, 'sounds', sound1 + '.')
+print(animal2, 'sounds', sound2 + '.')
+print(animal3, 'sounds', sound3 + '.')
