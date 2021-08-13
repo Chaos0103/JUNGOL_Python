c647ad760e090d995579a9f6341d45faa0baa6f3
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-08-13 23:11:14 +0900

    Create 형성평가_06.py

---
+++ LCoder_Python/연산자/형성평가_06.py
@@ -0,0 +1,8 @@
+per1_high = int(input())
+per1_weight = int(input())
+per2_high = int(input())
+per2_weight = int(input())
+
+result = (per1_high > per2_high) and (per1_weight > per2_weight)
+
+print(result)
