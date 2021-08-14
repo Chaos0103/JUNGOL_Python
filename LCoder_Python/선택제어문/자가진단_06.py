296d589fbd53e36852e9415d37d06bc4c8401449
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-08-14 20:54:06 +0900

    Create 자가진단_06.py

---
+++ LCoder_Python/선택제어문/자가진단_06.py
@@ -0,0 +1,12 @@
+sex, age = map(str, input().split())
+
+if sex == 'M':
+    if int(age) >= 18:
+        print('MAN')
+    else:
+        print('BOY')
+else:
+    if int(age) >= 18:
+        print('WOMAN')
+    else:
+        print('GIRL')
