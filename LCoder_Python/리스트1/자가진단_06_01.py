5af960e77e20ab8d3c06b3592e8fcefa03903ad4
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-08-14 19:56:56 +0900

    Create 자가진단_06_01.py

---
+++ LCoder_Python/리스트1/자가진단_06_01.py
@@ -0,0 +1,11 @@
+element = ['', '', '', '', '', '']
+
+element[0] = str(input('Element? '))
+element[1] = str(input('Element? '))
+element[2] = str(input('Element? '))
+element[3] = str(input('Element? '))
+element[4] = str(input('Element? '))
+element[5] = str(input('Element? '))
+index = int(input('Index? '))
+
+print(element[index:])
